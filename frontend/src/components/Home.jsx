45a33a5ae04257c094ef333763600b42831d1e4a
aridian <aridiannavarro03@gmail.com>
2023-11-07 19:16:29 +0000

    a3

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,27 +1,31 @@
 //Aridian Navarro Remedios
+// Importación de módulos y componentes necesarios
 import { Typography } from "@mui/material";
 import React from "react";
-import { useSelector } from 'react-redux'
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { loginActions } from "../store/storelogin";
+import { useSelector } from 'react-redux'// Selecciona el estado del store de Redux
+import { useDispatch } from "react-redux";// Permite despachar acciones a Redux
+import { useNavigate } from "react-router-dom";// Permite la navegación en la aplicación
+import { loginActions } from "../store/storelogin";// Acciones relacionadas con el inicio de sesión
 import { Button } from "@mui/material";
 import { useEffect } from 'react'
 
+// Definición del componente Home
+
 function Home() {
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const userData = useSelector(state => state.login)
-  const isLoggedin = userData.isAutenticated;
+  const dispatch = useDispatch()// Inicializa el hook de Redux
+  const navigate = useNavigate()// Inicializa el hook de navegación
+  const userData = useSelector(state => state.login)// Almacena el estado del usuario desde el store de Redux
+  const isLoggedin = userData.isAutenticated;// Verifica si el usuario ha iniciado sesión
 
+    // Efecto secundario para redirigir al usuario si no ha iniciado sesión
   useEffect(()=>{
     if (!isLoggedin){ 
         navigate('/')
     }
     }, [isLoggedin, navigate])
 
- 
+    // Función para manejar el cierre de sesión
     console.log('Datos del usuario en el store:', userData)
     const handleLogout = (e) => {
       
@@ -29,7 +33,7 @@ function Home() {
       navigate('/');
 };
 
-
+  // Renderizado del componente Home
     return (
       <div>
         <Typography variant="h1">Página home de Aridian y Navarro</Typography>
@@ -42,5 +46,5 @@ function Home() {
       </div>
     );
 }
-
+// Exporta el componente Home
 export default Home
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,19 +1,24 @@
 //Aridian navarro remedios
+// Importación de módulos y componentes necesarios
 import React, { useState } from 'react';
 import { Avatar, Box, Grid, Paper, TextField, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import LockIcon from '@mui/icons-material/Lock';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { loginActions } from '../store/storelogin';
+import { useDispatch } from 'react-redux';// Permite despachar acciones a Redux
+import { useNavigate } from 'react-router-dom';// Permite la navegación en la aplicación
+import { loginActions } from '../store/storelogin';// Acciones relacionadas con el inicio de sesión
 
+// Definición del componente de inicio de sesión
 function Login() {
     const dispatch = useDispatch();
     const navigate = useNavigate(); // Placed navigate here
 
+    // Estado local para almacenar los datos de inicio de sesión
     const [login, setLogin] = useState({ user: '', pass: '' });
 
+    // Función para verificar las credenciales del usuario
     const isVerifiedUser = () => {
+        // Realiza una llamada a la API para verificar las credenciales
         fetch(`http://localhost:3030/login?user=${login.user}&password=${login.pass}`)
             .then(response => response.json())
             .then(response => {
@@ -21,20 +26,23 @@ function Login() {
                     if (Object.keys(response.data).length === 0) {
                         console.log('Datos incorrectos');
                     } else {
+                        // Despacha la acción de inicio de sesión con Redux
                         console.log(response);
                         dispatch(loginActions.login({
                             name: response.data.nombre,
                             rol: response.data.rol
                         }));
+                        // Navega a la página de inicio
                         navigate('/home');
                     }
                 }
             });
     };
-
+    // Función para manejar el envío del formulario
     const handleSubmit = (e) => {
         e.preventDefault();
         if (login.user.length !== 0 && login.pass.length !== 0) {
+            // Función para manejar el envío del formulario
             fetch(`http://localhost:3030/login?user=${login.user}&password=${login.pass}`)
                 .then(response => response.json())
                 .then(response => {
@@ -50,8 +58,8 @@ function Login() {
                                     rol: response.data.rol
                                 }));
                                 
-                               
-    navigate('/home');
+                               // Navega a la página de inicio
+                                navigate('/home');
                             }
                         }
                     }
@@ -62,7 +70,7 @@ function Login() {
         }
     };
 
-
+        // Renderizado del componente de inicio de sesión
     return (
         <Grid container justifyContent={'center'} alignItems={'center'} style={{ minHeight: '100vh' }}>
             <Grid item xs={3} md={3} l={2} xl={2}>
@@ -103,5 +111,5 @@ function Login() {
         </Grid>
     )
 }
-
+// Exporta el componente de inicio de sesión
 export default Login;
